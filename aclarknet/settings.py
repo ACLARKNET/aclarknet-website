108baf6f9f5c2ae1efdbc6220c8ed43cd9e99e7f
Alex Clark <aclark@aclark.net>
2016-01-01 21:07:53 -0500

    Update

--- aclarknet/settings.py
+++ aclarknet/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aclarknet.website',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -86,14 +87,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME':
         'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
+    #    {
+    #        'NAME':
+    #        'django.contrib.auth.password_validation.MinimumLengthValidator',
+    #    },
+    #    {
+    #        'NAME':
+    #        'django.contrib.auth.password_validation.CommonPasswordValidator',
+    #    },
     {
         'NAME':
         'django.contrib.auth.password_validation.NumericPasswordValidator',
--- aclarknet/urls.py
+++ aclarknet/urls.py
@@ -14,7 +14,12 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from aclarknet.website import views
 from django.conf.urls import url
 from django.contrib import admin
 
-urlpatterns = [url(r'^admin/', admin.site.urls), ]
+urlpatterns = [
+    url(r'^admin/', admin.site.urls), url(r'^$',
+                                          views.home,
+                                          name='home')
+]
