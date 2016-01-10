8405b9412d2a175ea93e05f3dafd7d86091694f8
Alex Clark <aclark@aclark.net>
2016-01-10 11:45:18 -0500

    Update

--- aclarknet/website/admin.py
+++ aclarknet/website/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Client
+from .models import Service
+from .models import TeamMember
+from .models import Testimonial
 
 # Register your models here.
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    """
+    """
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    """
+    """
+
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    """
+    """
+
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    """
+    """
--- aclarknet/website/models.py
+++ aclarknet/website/models.py
@@ -3,3 +3,39 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Client(models.Model):
+    icon = models.CharField(default=None, max_length=30, null=True)
+    name = models.CharField(default=None, max_length=90)
+    text = models.TextField(default=None)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=60)
+    icon = models.CharField(default=None, max_length=30)
+    text = models.TextField(default=None)
+
+    def __unicode__(self):
+        return self.name
+
+
+class TeamMember(models.Model):
+    name = models.CharField(max_length=60)
+    icon = models.ImageField()
+    text = models.TextField(default=None)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Testimonial(models.Model):
+    name = models.CharField(max_length=120)
+    text = models.TextField()
+    date = models.DateTimeField(default=None)
+
+    def __unicode__(self):
+        return self.name
