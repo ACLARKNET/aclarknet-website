a83a673ec65e906a93113d8893e3f9d2143c50f5
Alex Clark <aclark@aclark.net>
2016-01-10 12:13:17 -0500

    Update

--- aclarknet/website/views.py
+++ aclarknet/website/views.py
@@ -15,6 +15,11 @@ def about(request):
     return render(request, 'about.html', context)
 
 
+def book(request):
+    context = {}
+    return render(request, 'book.html', context)
+
+
 def clients(request):
     context = {}
     clients = Client.objects.all()
