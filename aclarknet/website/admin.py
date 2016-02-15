6785544058423abf3fa1970d4dc20a04e41ebc52
Alex Clark <aclark@aclark.net>
2016-02-15 18:21:05 -0500

    Update

--- aclarknet/website/admin.py
+++ aclarknet/website/admin.py
@@ -1,25 +1,3 @@
 from django.contrib import admin
-from .models import Developer
-from .models import Page
-from .models import Partner
 
 # Register your models here.
-
-
-@admin.register(Developer)
-class DeveloperAdmin(admin.ModelAdmin):
-    """
-    """
-
-
-@admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
-    """
-    """
-    prepopulated_fields = {"slug": ("name", )}
-
-
-@admin.register(Partner)
-class PartnerAdmin(admin.ModelAdmin):
-    """
-    """
